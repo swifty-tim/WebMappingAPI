3f1c3ff7a3b381908ede8d5344d0ebbfd7dddce6
Timothy Barnard <timothy.barnard@mydit.ie>
2017-04-12 00:18:53 +0100

    models added

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,9 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import  User, Event, Attendees
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(Attendees)
