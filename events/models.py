acbc993538ebe6d4f5669a2b3f09466bceda7749
Timothy Barnard <timothy.barnard@mydit.ie>
2017-04-13 11:43:33 +0100

    models created1

--- events/models.py
+++ events/models.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
+#from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
@@ -85,7 +85,7 @@ class Attendees(models.Model):
         return "{} owned by {}".format(self.attendee.first_name, self.event.name)
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+#@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+#def create_auth_token(sender, instance=None, created=False, **kwargs):
+#    if created:
+#        Token.objects.create(user=instance)
