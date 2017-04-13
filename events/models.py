4b2e8914c020273e36ca487dcc02b3c1eb0db328
Timothy Barnard <timothy.barnard@mydit.ie>
2017-04-13 14:29:23 +0100

    models created

--- events/models.py
+++ events/models.py
@@ -7,15 +7,15 @@ from django.contrib.gis.geos import Point
 
 from django.contrib.gis.db import models
 from django.contrib.gis import geos
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-#from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token
 
 
-class User(AbstractUser):
+class User(User):
     created = models.DateTimeField(
         auto_now_add=True
     )
@@ -85,7 +85,7 @@ class Attendees(models.Model):
         return "{} owned by {}".format(self.attendee.first_name, self.event.name)
 
 
-#@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-#def create_auth_token(sender, instance=None, created=False, **kwargs):
-#    if created:
-#        Token.objects.create(user=instance)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
