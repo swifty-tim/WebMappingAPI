8c4e33a47707ee190789a41694ecd24525bc65b3
Timothy Barnard <timothy.barnard@mydit.ie>
2017-04-15 20:39:56 +0100

    include id4

--- events/serializers.py
+++ events/serializers.py
@@ -2,6 +2,7 @@ from . import models
 from EventAPI import settings
 from rest_framework import serializers
 from rest_framework_gis import serializers as geo_serializers
+from rest_framework_gis import serializers
 from rest_framework.reverse import reverse
 from django.contrib.auth import get_user_model
 from django.contrib.gis.geos import GEOSGeometry, LineString, Point, Polygon
@@ -16,9 +17,8 @@ class EventSerializer(geo_serializers.GeoFeatureModelSerializer):
 
 
 
-class AttendeesSerializer(geo_serializers.GeoFeatureModelSerializer):
+class AttendeesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Attendees
-        geo_field = "location"
         fields = ('id', 'attendee', 'event')
